ef23d516525d7c82a65aaf5885807e47ccb7bfe5
Wei Lin Ong <ongweilin93@gmail.com>
2022-06-21 22:28:57 +0800

    testing

--- client/src/routes/Order-page/Order-page.route.jsx
+++ client/src/routes/Order-page/Order-page.route.jsx
@@ -12,9 +12,6 @@ export default function OrderPage() {
 
   const ann1 = <div className='announcement'>No order yet.</div>;
   const ann2 = <div className='announcement'>Loading...</div>;
-  const orders = allOrders.map((order) => (
-    <OrderItem key={order._id} order={order} />
-  ));
 
   return (
     <div>
@@ -25,7 +22,19 @@ export default function OrderPage() {
         <h4>status</h4>
         <h4>refund</h4>
       </div>
-      {allOrders ? (allOrders.length > 0 ? orders : ann2) : ann1}
+      {allOrders ? (
+        allOrders.length > 0 ? (
+          <>
+            {allOrders.map((order) => (
+              <OrderItem key={order._id} order={order} />
+            ))}
+          </>
+        ) : (
+          ann2
+        )
+      ) : (
+        ann1
+      )}
     </div>
   );
 }
