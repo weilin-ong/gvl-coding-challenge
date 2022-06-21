7565fe1e56de9e2b8e4a0e020b84a4f4d399124f
Wei Lin Ong <ongweilin93@gmail.com>
2022-06-21 22:12:45 +0800

    refactor order page

--- client/src/routes/Order-page/Order-page.route.jsx
+++ client/src/routes/Order-page/Order-page.route.jsx
@@ -10,6 +10,12 @@ export default function OrderPage() {
     getAllOrders().then((data) => setAllOrders(data));
   }, []);
 
+  const ann1 = <div className='announcement'>No order yet.</div>;
+  const ann2 = <div className='announcement'>Loading...</div>;
+  const orders = allOrders.map((order) => (
+    <OrderItem key={order._id} order={order} />
+  ));
+
   return (
     <div>
       <Navbar />
@@ -19,12 +25,7 @@ export default function OrderPage() {
         <h4>status</h4>
         <h4>refund</h4>
       </div>
-      {allOrders.length === 0 && <div>No order yet.</div>}
-      {allOrders.length > 0 ? (
-        allOrders.map((order) => <OrderItem key={order._id} order={order} />)
-      ) : (
-        <div>loading...</div>
-      )}
+      {allOrders.length ? (allOrders.length > 0 ? orders : ann2) : ann1}
     </div>
   );
 }
